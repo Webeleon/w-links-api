1251405e4593177a0a37b7bab47ecf577b2a0c95
Julien Prugne <julien@webeleon.dev>
2022-04-30 07:11:00 +0200

    chore: clever config compat

--- src/configurations/database.config.ts
+++ src/configurations/database.config.ts
@@ -2,9 +2,12 @@ import { registerAs } from '@nestjs/config';
 
 export const databaseConfig = registerAs('database', () => ({
   type: process.env.DB_TYPE || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'w_links_api',
+  host: process.env.DB_HOST || process.env.POSTGRESQL_ADDON_HOST || 'localhost',
+  port: process.env.DB_PORT || process.env.POSTGRESQL_ADDON_PORT || 5432,
+  username:
+    process.env.DB_USERNAME || process.env.POSTGRESQL_ADDON_USER || 'postgres',
+  password:
+    process.env.DB_PASSWORD || process.env.POSTGRESQL_ADDON_PASSWORD || '',
+  database:
+    process.env.DB_DATABASE || process.env.POSTGRESQL_ADDON_DB || 'w_links_api',
 }));
